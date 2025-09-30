1f7d1c5735c81a6ea3aa1e8a183d5523912648f0
Karthikeyan <capriconor01@gmail.com>
2025-09-30 22:11:22 +0530

    Time: 0 ms (100%), Space: 12.5 MB (59.69%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        str_arr_1=strs[0]
+        for str_arr_2 in strs:
+            while not str_arr_2.startswith(str_arr_1):
+                str_arr_1 =str_arr_1[:-1]
+                if not str_arr_1:
+                    return ""
+        return str_arr_1
+        
