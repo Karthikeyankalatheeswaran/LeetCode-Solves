3447b8133e9ab6fb090f0f212b15e5c4a66cac1a
Karthikeyan <capriconor01@gmail.com>
2025-10-24 22:31:02 +0530

    Time: 3 ms (68.47%), Space: 13.9 MB (90.13%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        single_number = 0
+    
+        for num in nums:
+            single_number ^= num  
+            
+        return single_number
