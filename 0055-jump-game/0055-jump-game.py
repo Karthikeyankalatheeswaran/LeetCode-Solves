451a6da4c876db99471395579aa07310365ba195
Karthikeyan <capriconor01@gmail.com>
2025-11-20 22:59:47 +0530

    Time: 33 ms (52.72%), Space: 13.1 MB (94.69%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -7,14 +7,12 @@ class Solution(object):
         max_reach = 0
         
         for i, jump in enumerate(nums):
-            # If we can't reach this index, we can't go further
             if i > max_reach:
                 return False
             
-            # Update the furthest we can reach
+
             max_reach = max(max_reach, i + jump)
-            
-            # Optimization: Early exit if we can already reach the end
+        
             if max_reach >= len(nums) - 1:
                 return True
                 
