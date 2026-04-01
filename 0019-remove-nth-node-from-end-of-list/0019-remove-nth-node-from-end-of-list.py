1734e4ea3f04e177020bd72411c7bdf6f0e47444
Karthikeyan <capriconor01@gmail.com>
2026-04-01 16:13:31 +0530

    Time: 0 ms (100%), Space: 12.3 MB (78.11%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,26 +1,27 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
     def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        if not head:
+            return False
+
         dummy = ListNode(0,head)
-        fast = dummy
-        slow = dummy
-
-#This loop is to skip the fast pointer ahead of 2 steps from slow pointer
-        for i in range (n+1):
-            fast = fast.next
-
-#This loop traverses the ll
-        while fast is not None:
-            slow = slow.next
-            fast = fast.next
-#When the fast pointer is none or gone out of loop
-        slow.next = slow.next.next
-    #this deleted the n-th node
-
-        return dummy.next
+        slow = fast = dummy
         
+        for _ in range(n + 1):
+            fast = fast.next
+
+        while fast:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+        return dummy.next
