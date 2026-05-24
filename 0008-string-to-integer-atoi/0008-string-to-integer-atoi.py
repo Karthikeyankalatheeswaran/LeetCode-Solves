31c2a8e131f22221cde41466a93dc1a4edaf8d97
Karthikeyan <capriconor01@gmail.com>
2026-05-24 22:42:39 +0530

    Time: 2 ms (68.75%), Space: 12.3 MB (57.51%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        
+        i, n = 0, len(s)
+        sign = 1
+        result = 0
+        
+
+        while i < n and s[i] == ' ':
+            i += 1
+
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+            
+        while i < n and s[i].isdigit():
+            digit = int(s[i])
+
+            if result > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+                
+            result = result * 10 + digit
+            i += 1
+            
+        return sign * result
