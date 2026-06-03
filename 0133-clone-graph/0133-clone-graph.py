87605c62f572a7e462c20e88aabeda629587c459
Karthikeyan <capriconor01@gmail.com>
2026-06-04 00:56:33 +0530

    Time: 37 ms (41.72%), Space: 12.8 MB (31.97%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        clone_copy = {}
+
+        def dfs(node):
+            if node in clone_copy:
+                return clone_copy[node]
+
+            copy = Node(node.val)
+            clone_copy[node] = copy
+
+            for neighbor in node.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            return copy 
+
+        return dfs(node) if node else None
