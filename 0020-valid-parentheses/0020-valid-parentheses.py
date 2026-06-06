ca4c32c10fb71c7f185c44290be2c8329b32b426
Karthikeyan <capriconor01@gmail.com>
2026-06-06 12:17:39 +0530

    Time: 4 ms (42.52%), Space: 12.5 MB (69.13%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,19 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        hashmap = {")": "(", "}": "{", "]": "["}
-        stk = []
- 
-        for c in s:
-            if c not in hashmap:
-                stk.append(c)
-            else:
-                if not stk:
-                    return False
-                else:
-                    popped = stk.pop()
-                    if popped != hashmap[c]:
-                        return False
- 
-        return not stk
         
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+
+            else:
+                stack.append(c)
+                
+        return True if not stack else False
