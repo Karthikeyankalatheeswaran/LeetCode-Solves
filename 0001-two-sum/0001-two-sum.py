93b2ec98edfa119e281e8d46fb7d94f6f15e93d7
Karthikeyan <capriconor01@gmail.com>
2026-06-15 21:22:02 +0530

    Time: 1 ms (61.38%), Space: 13 MB (67.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,11 +7,10 @@ class Solution(object):
         """
         seen = {}
 
-        for i,num in enumerate(nums):
+        for i , num in enumerate(nums):
             complement = target - num
             if complement in seen:
                 return [seen[complement],i]
             seen[num] = i
 
         return []
-                
